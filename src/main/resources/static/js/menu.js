943d7bb790cdef15526a24d0b3b410a86ed9223c
geonwoo0928 <ddolikim6770@gmail.com>
2024-05-06 23:57:42 +0900

    주문 내역 미완

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,10 +1,4 @@
-function addToOrder(menuName, menuPrice) {
-    var menuOrderView = document.querySelector('.menu-order-view');
-
-    // 메뉴 정보를 표시할 새로운 요소를 생성합니다.
-    var menuItem = document.createElement('div');
-    menuItem.textContent = menuName + ' - ' + menuPrice + '원';
-
-    // 생성한 요소를 주문 내역 창에 추가합니다.
-    menuOrderView.appendChild(menuItem);
-}
+ function addToOrder(menuName, menuPrice) {
+        var orderView = document.querySelector('.menu-order-view');
+        orderView.innerHTML += "<div>" + menuName + " - " + menuPrice + "원</div>";
+ } //주문내역 보여주는 js
